7c508cda65edaeb2a0ecb6d197423816d48bfbab
yashaswini-05506 <yashaswinimadnallimath@gmail.com>
2025-12-02 11:51:59 +0530

    Update test_vehicle.py

--- test_vehicle.py
+++ test_vehicle.py
@@ -1,14 +1,14 @@
-from vehicle import vehicle_details
+from vehical import vehicle_details
 
 def test_vehicle_details():
-    vehicle_number = "0137"
+    vehicle_number = "KA-01-1234"
     owner_name = "yashaswini"
-    vehicle_type = "Car"
+    vehicle_type = "two wheeler"
     year_of_manufacture = 2022
     expected_output = (
-        "Vehicle Number: 0137\n"
+        "Vehicle Number: KA-01-1234\n"
         "Owner Name: yashaswini\n"
-        "Vehicle Type: Car\n"
+        "Vehicle Type: two wheeler\n"
         "Year of Manufacture: 2022\n"
     )
     actual_output = vehicle_details(vehicle_number, owner_name, vehicle_type, year_of_manufacture)
